14681c5b35f36203168e7877b736391c40698914
hugosenari <hugosenari@gmail.com>
2022-03-12 00:33:38 -0300

    chore: re index asciitext

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Ascii Text allows you to print large ASCII fonts for the console and for the web'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."asciitext-master".url = "path:./master";
+  inputs."asciitext-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."asciitext-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
